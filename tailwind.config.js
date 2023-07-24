54fe906ab42f52e12d199527114827408c1f561d
Kamrul Hasan <kamrul3496hasan@gmail.com>
2023-07-24 22:05:25 +0600

    Added All Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+          primary: "#F00",
+          primary2: "#FF8938 ",
+          "title-color": "#1C1C1C",
+          "desc-color": "#777",
+          "desc-color2": "#F4F4F4",
+        },
+        fontFamily: {
+          manrope: ["Manrope", "sans-serif"],
+        },
+        maxWidth: {
+          standard: "1140px",
+          secondary: "1250px",
+        },
+      },
     },
     plugins: [],
   }
